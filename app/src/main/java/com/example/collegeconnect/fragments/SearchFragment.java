96836a5ee8461b065bb68918e977e1d549d05a52
janelle-cheung <cheungj19@gmail.com>
2021-08-02 16:59:46 -0400

    Setup layout for college filtering

--- app/src/main/java/com/example/collegeconnect/fragments/SearchFragment.java
+++ app/src/main/java/com/example/collegeconnect/fragments/SearchFragment.java
@@ -1,15 +1,12 @@
 package com.example.collegeconnect.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -18,6 +15,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.collegeconnect.CollegeAIClient;
@@ -26,7 +33,9 @@ import com.example.collegeconnect.activities.SearchResultActivity;
 import com.example.collegeconnect.adapters.CollegeSuggestionsAdapter;
 import com.example.collegeconnect.databinding.FragmentSearchBinding;
 import com.example.collegeconnect.models.College;
+import com.google.android.material.slider.Slider;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -40,6 +49,8 @@ public class SearchFragment extends Fragment implements CollegeSuggestionsAdapte
 
     public static final String TAG = "SearchFragment";
     public static final String KEY_SEARCH_FRAG_COLLEGE_ID = "SearchFrag collegeId";
+    private static final int MIN_SAT_SCORE = 600;
+    private static final int MIN_ACT_SCORE = 12;
     private ArrayAdapter<String> arrayAdapter;
     private FragmentSearchBinding binding;
     private CollegeSuggestionsAdapter adapter;
@@ -56,6 +67,7 @@ public class SearchFragment extends Fragment implements CollegeSuggestionsAdapte
 
         configureAutocompleteSearch();
         configureSuggestionsByCategory();
+        configureFilterDialog();
 
         // When user clicks on search button, open search result activity -> college details fragment
         binding.ibSearch.setOnClickListener(new View.OnClickListener() {
@@ -199,6 +211,126 @@ public class SearchFragment extends Fragment implements CollegeSuggestionsAdapte
 
             }
         });
+    }
+
+    private void configureFilterDialog() {
+        binding.btnFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+                final View filterAlertDialog = getLayoutInflater().inflate(R.layout.filter_alert_dialog, null);
+                builder.setView(filterAlertDialog);
+
+                CheckBox cbxPublic = filterAlertDialog.findViewById(R.id.cbxPublic);
+                CheckBox cbxPrivate = filterAlertDialog.findViewById(R.id.cbxPrivate);
+                CheckBox cbx4Year = filterAlertDialog.findViewById(R.id.cbx4Year);
+                CheckBox cbx2Year = filterAlertDialog.findViewById(R.id.cbx2Year);
+                TextView tvSliderCost = filterAlertDialog.findViewById(R.id.tvSliderCost);
+                Slider sliderCost = filterAlertDialog.findViewById(R.id.sliderCost);
+                TextView tvSliderSAT = filterAlertDialog.findViewById(R.id.tvSliderSAT);
+                Slider sliderSAT = filterAlertDialog.findViewById(R.id.sliderSAT);
+                TextView tvSliderACT = filterAlertDialog.findViewById(R.id.tvSliderACT);
+                Slider sliderACT = filterAlertDialog.findViewById(R.id.sliderACT);
+                Button btnSmall = filterAlertDialog.findViewById(R.id.btnCollegeSmall);
+                Button btnMedium = filterAlertDialog.findViewById(R.id.btnCollegeMedium);
+                Button btnLarge = filterAlertDialog.findViewById(R.id.btnCollegeLarge);
+
+                btnSmall.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        btnSmall.setSelected(!btnSmall.isSelected());
+                    }
+                });
+
+                btnMedium.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        btnMedium.setSelected(!btnMedium.isSelected());
+                    }
+                });
+
+                btnLarge.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        btnLarge.setSelected(!btnLarge.isSelected());
+                    }
+                });
+
+                // If max net cost slider is set to 0, make slider gray and don't filter with max net cost
+                sliderCost.addOnChangeListener(new Slider.OnChangeListener() {
+                    @Override
+                    public void onValueChange(@NonNull @NotNull Slider slider, float value, boolean fromUser) {
+                        String maxNetCostPrompt = getString(R.string.college_cost);
+                        tvSliderCost.setText(String.format("%s: $%s", maxNetCostPrompt, (int) value));
+                        if (value > 0) {
+                            sliderCost.setThumbStrokeColor(ColorStateList.valueOf(0x04C4CFF));
+                        } else {
+                            sliderCost.setThumbStrokeColor(ColorStateList.valueOf(0xFFAAAAAA));
+                        }
+                    }
+                });
+
+                // If SAT slider is set to 600, make slider gray and don't filter with SAT score
+                sliderSAT.addOnChangeListener(new Slider.OnChangeListener() {
+                    @Override
+                    public void onValueChange(@NonNull @NotNull Slider slider, float value, boolean fromUser) {
+                        tvSliderSAT.setText(String.format("SAT: %s", (int) value));
+                        if (value > MIN_SAT_SCORE) {
+                            sliderSAT.setThumbStrokeColor(ColorStateList.valueOf(Color.parseColor("#4C4CFF")));
+                        } else {
+                            sliderSAT.setThumbStrokeColor(ColorStateList.valueOf(Color.parseColor("#FFAAAAAA")));
+                        }
+                    }
+                });
+
+                // If ACT slider is set to 12, make slider gray and don't filter with ACT score
+                sliderACT.addOnChangeListener(new Slider.OnChangeListener() {
+                    @Override
+                    public void onValueChange(@NonNull @NotNull Slider slider, float value, boolean fromUser) {
+                        tvSliderACT.setText(String.format("ACT: %s", (int) value));
+                        if (value > MIN_ACT_SCORE) {
+                            sliderACT.setThumbStrokeColor(ColorStateList.valueOf(Color.parseColor("#4C4CFF")));
+                        } else {
+                            sliderACT.setThumbStrokeColor(ColorStateList.valueOf(Color.parseColor("#FFAAAAAA")));
+                        }
+                    }
+                });
+
+                // Apply button: send data from the AlertDialog to the Activity
+                builder.setPositiveButton(getString(R.string.apply), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                sendDialogDataToFragment(cbxPublic.isChecked(), cbxPrivate.isChecked(),
+                                                        cbx4Year.isChecked(), cbx2Year.isChecked(),
+                                                        (int) sliderCost.getValue(),
+                                                        (int) sliderSAT.getValue(),
+                                                        (int) sliderACT.getValue());
+                            }
+                        }
+                );
+
+                // Cancel button
+                builder.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                // Show dialog
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setAllCaps(false);
+            }
+        });
+    }
+
+    private void sendDialogDataToFragment(boolean filterPublic, boolean filterPrivate,
+                                          boolean filter4Year, boolean filter2Year,
+                                          int maxCost, int SAT, int ACT) {
+        Toast.makeText(getContext(), String.valueOf(maxCost), Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), String.valueOf(filterPublic), Toast.LENGTH_SHORT).show();
     }
 
     private void launchSearchResultActivity(String collegeId) {
